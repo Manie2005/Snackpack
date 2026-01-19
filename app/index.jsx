5ccb7aa20d3d717ed8e42b6dc1aa21fcccec3182
Immanuel <maniesamuel24@gmail.com>
2026-01-20 00:26:15 +0100

    App Update

---
+++ app/index.jsx
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import Logo from '../assets/snackpack.png'
+import { Link } from 'expo-router'
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+    <Image source={Logo} style={styles.img} resizeMode='contain' />
+          <Text style ={styles.title}>Welcome everyone</Text>
+            <Text style ={styles.secondtitle}>to the one-stop app for your treats</Text>
+            <Text style ={styles.card}><Link href= "/about">About Us</Link> </Text>
+
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+justifyContent:'center',
+alignItems:'center',
+backgroundColor:'#fff'
+    },
+
+    title:{
+fontWeight:'bold',
+fontSize:24
+    },
+ secondtitle:{
+marginTop:10,
+marginBottom:5
+    },
+card:{
+    backgroundColor: `#E10600`,
+    marginTop:15,
+    padding:15,
+    borderRadius:15,
+    fontWeight:'bold'
+},
+img:{
+marginBottom:10,
+height:200,
+width:`100%`,
+}
+
+})
