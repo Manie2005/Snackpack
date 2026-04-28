a2d192f7ba22308154f056641947e243905e9352
Immanuel <maniesamuel24@gmail.com>
2026-04-28 13:02:19 +0100

    Updated Commit

---
+++ app/(auth)/congrats.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import CongratsLogo from "../../assets/congrats.png";
+
+const congrats = () => {
+  return (
+    <View>
+      <Text>congrats</Text>
+    </View>
+  )
+}
+
+export default congrats
+
+const styles = StyleSheet.create({})
